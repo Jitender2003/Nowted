1b2a3c5ff98d1758b0795b0c013e0e3ca9ff2133
Jitender Singh <jitenderrautela0@gmail.com>
2025-09-09 12:02:33 +0530

    Add interface

---
+++ src/types/interface.ts
@@ -0,0 +1,30 @@
+export interface FolderData {
+  id: string;
+  name: string;
+  created_at: string;
+  updated_at: string;
+}
+export type FoldersResponseData = FolderData[];
+
+export interface NoteData {
+  note: {
+    id: string;
+    name: string;
+    content: string;
+    isFavorite: boolean;
+    isArchived: boolean;
+    isDeleted: boolean;
+    updatedAt: string;
+    folder: {
+      id: string;
+      name: string;
+      isDeleted: boolean;
+      createdAt: string;
+      updatedAt: string;
+    };
+  };
+}
+
+export interface RecentNotesResponseData {
+  recentNotes: NoteData[];
+}
